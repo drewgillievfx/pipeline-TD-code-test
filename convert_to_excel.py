c739d7cbf8b871177f776a24dec2567543c8c755
drewgillievfx <drewgillievfx@gmail.com>
2023-01-25 12:47:59 -0500

    Create convert_to_excel.py
    Starting file, adding comment block and basic structure for code

---
+++ convert_to_excel.py
@@ -0,0 +1,60 @@
+"""
+    convert_to_excel.py
+    Start Date: 20230125
+    Version: 1.0
+    Written by: Drew Gillie
+
+    This code is written for the purpose of completing a coding challenge for
+    Laika for the Media Systems Pipeline Technical Director Position
+
+    ##########################################################################
+    Challenge #1
+
+    Write a script that exports the data in a Python dictionary to Microsoft 
+    Excel in spreadsheet format. The dictionary is the result of a database 
+    query and is provided to you in pickle file called test_data.pkl. The 
+    spreadsheet should include columns for:
+        - Task Name, 
+        - Set Part Name, 
+        - Parent Build Name, 
+        - Start Date, and 
+        - End Date. 
+    The data should be sorted by earliest Start Date first. 
+    
+    If a Due Date has already passed, shade the entire row red in Excel.
+
+    You'll need to install and use an external Python library to handle the 
+    Excel import. We use `openpyxl` for this at Laika.
+
+    The database returns unfriendly key names for some fields. We map these 
+    keys to the above listed human-readable fields in the following way:
+        - Task Name = `content`
+        - Set Part = `entity`
+        - Parent Build = `sg_parent_build`
+        - Start Date = `start_date`
+        - End Date = `due_date`
+    ##########################################################################
+
+
+    This program will convert data from a pickle file into an excel file.
+    Only relevant data is brought over and sorted from earliest date
+    
+    Overall thought process:
+        1. Load the data from pickle
+        2. Sort data by date
+        3. Create an excel file
+        4. Add headers
+        5. Bring data in, sorted by date
+        6. Highlight past dates in red
+
+"""
+##############################################################################
+# 1. Unpickle file and place into a dict
+##############################################################################
+# 2. Sort data
+##############################################################################
+# 3. Create an excel file
+##############################################################################
+# 4. Add data to excel file
+##############################################################################
+# 5. Format data
