da5ed8ea2e2a20424398d1da7c4318b5ad70f736
drewgillievfx <drewgillievfx@gmail.com>
2023-01-26 12:00:42 -0500

    Update basic_dialog.py
    Working version of code using PyQt5 instead of 4 as the instructions state.
    
    Also note that this is an outdated assignment and I was instructed to use the newer version.

--- basic_dialog.py
+++ basic_dialog.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
-from PyQt6 import QtGui
+# from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
 
-class BasicDialog(QtGui.QDialog):
+class BasicDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super(BasicDialog, self).__init__(parent=parent)
-        #Instantiate a layout and set it as the dialogs main layout
-        main_layout = QtGui.QVBoxLayout()
+        # Instantiate a layout and set it as the dialogs main layout
+        main_layout = QtWidgets.QVBoxLayout()
         self.setLayout(main_layout)
-        #Add ok and cancel buttons
-        #And hook them up to dialogs accept/reject methods
-        button_box = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok|\
-                                            QtGui.QDialogButtonBox.Cancel)
+        # Add ok and cancel buttons
+        # And hook them up to dialogs accept/reject methods
+        button_box = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Ok|\
+                                            QtWidgets.QDialogButtonBox.Cancel)
         button_box.accepted.connect(self.accept)
         button_box.rejected.connect(self.reject)
         main_layout.addWidget(button_box)
@@ -20,12 +22,13 @@ class BasicDialog(QtGui.QDialog):
         return None
 
 if __name__ == '__main__':
-    #Instantiate a QApplication - requirement for Qt
-    app = QtGui.QApplication([])
-    #Instantiate, show, then raise our dialog to the front
+    # Instantiate a QApplication - requirement for Qt
+    app = QtWidgets.QApplication([])
+    # Instantiate, show, then raise our dialog to the front
     dlg = BasicDialog()
     dlg.show()
     dlg.raise_()
-    #only getValues if 'Ok' is clicked
+    # only getValues if 'Ok' is clicked
     if dlg.exec_():
-        print dlg.getValues()
+        values = dlg.getValues()
+        print(values)
