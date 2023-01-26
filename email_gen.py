c3261583dd3846a97afd0afc269f9ed42077e1f0
drewgillievfx <drewgillievfx@gmail.com>
2023-01-26 13:07:53 -0500

    cPickle issue
    cPickle does not work with python3.
    just pickle

--- email_gen.py
+++ email_gen.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import cPickle as pickle
+import pickle
 
 def getItemString(item):
     if item['content'] == 'design':
@@ -9,7 +9,11 @@ def getItemString(item):
     item_string = 'Task Date Change\n'
     item_string += 'Task Name: %s\n' % task_name
     item_string += 'Link: %s\n' % item['entity']['code'][0]
-    item_string += 'Parent Build: %s\n' % item['sg_parent_build']['code'] if item['sg_parent_build'] else ''
+     
+    if item['sg_parent_build']:
+        item_string += 'Parent Build: %s\n' % item['sg_parent_build']['code']
+    else:
+         'Not available'
     item_string += 'Start: %s\n' % item['start_date']
     item_string += 'End: %s\n' % item['due_date']
     return item_string
@@ -43,4 +47,13 @@ if __name__ == '__main__':
         potential issue line 21: email_body = ''
         definite issue line 26: print email_body
             should be print(email_body)
+    4. Lets look at the getItemString function
+        potentially line 12: with the if statement
+            I will move it down a line to fit within Pep 8
+            This is a simpler line that could be all on one line, 
+            but to keep in the same style, I will change it
+    5. Didnt catch on first inspection
+        this probably was not the problem
+        but cPickle does not work in Python3
+        changed to pickle
 """
