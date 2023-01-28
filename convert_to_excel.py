76a2c2674af06d692c4b9a59df7f028a75d0e072
drewgillievfx <drewgillievfx@gmail.com>
2023-01-28 01:17:11 -0500

    going down to basics on code
    changing too many variables. going section by section to debug.
    
    already found an issue with sending data to excel. using a list item to string for excel.
    
    excel wont take list items

--- convert_to_excel.py
+++ convert_to_excel.py
@@ -65,287 +65,50 @@ from openpyxl.styles import PatternFill
 import sys
 
 ##############################################################################
-# 5. Format data
-""" This section of code is dedicated to reformatting the data on excel. """
+""" Variables to change for different type of data sheets. """
+column_names = ['Task', 'Set Part', 'Parent Build', 'Start Date', 'End Date']
 
-
-def fill_row(selected_row):  # Give a row number to fill in red.
-    row_cells = ws[selected_row]  # Select row from input.
-
-    # Select the color red.
-    fill_color = PatternFill(start_color='FFC7CE',
-                             end_color='FFC7CE', fill_type='solid')
-    font_change = Font(name='Arial', size=12, color='8B0000')
-
-    # Set the fill color of all of the cells in the row.
-    for cell in row_cells:
-        cell.fill = fill_color
-        cell.font = font_change
-
-
-def date_to_check_from_excel():  # checks date, colors row red.
-    for dates in range(2, (list_size+2)):
-        # Get date from excel.
-        date_to_get_checked = ws.cell(row=dates, column=5).value
-        listed_date = datetime.strptime(date_to_get_checked, '%Y-%m-%d')
-
-        # Get current date.
-        current_date = datetime.now()
-        # print(current_date)  # Only for testing purposes---.
-        # print(listed_date)  # Only for testing purposes---.
-        if listed_date < current_date:
-            # print('This date has already passed.')  # Testing purposes---.
-            fill_row(dates)
-        else:
-            # print('This date has not yet passed.')  # Testing purposes---.
-            continue
-
-
-def format_title():  # Format the titles / headers.
-    # Set the Title font.
-    title_font = Font(name='Arial', size=20, color='ffffff')
-
-    # Set Title fill.
-    title_fill = PatternFill(start_color='000000', end_color='000000',
-                             fill_type='solid')
-
-    # Iterate through column_names.
-    for i, title in enumerate(column_names):
-        column_letter = get_column_letter(i + 1)  # Get the column letter.
-        column_number = column_letter + '1'
-
-        # Apply the formatting.
-        ws[column_number].font = title_font
-        ws[column_number].fill = title_fill
-
-
-def center_align_cells(worksheet):  # Alaign data to center.
-    for row in worksheet.iter_rows():
-        for cell in row:
-            cell.alignment = Alignment(horizontal='center')
-
-
-def right_align_column_a():  # Aligns first column data to right.
-    for row in ws.iter_rows(min_row=1, max_col=1):
-        for cell in row:
-            if cell.row != 1:
-                cell.alignment = Alignment(horizontal='right',
-                                           vertical='center')
-
-
-def autofit_columns(column_number):  # Adjust column width based on max cell.
-    max_size = 0  # Initialize the max size.
-
-    # Iterate through cells in the column.
-    for cell in ws[column_number]:
-        # Get the length of the cell value and font size.
-        length = len(str(cell.value))
-        font_size = cell.font.size
-        max_size = max(max_size, length + font_size)
-
-    average_size = 15  # Guess and check to see how it looks.
-    new_column_size = ((average_size + max_size) / 2) - 2
-
-    # print(max_size)  # Only for testing purposes---.
-    ws.column_dimensions[column_number].width = new_column_size
-
-
-def check_unavailable_data():  # Search for data with no entry point.
-    # Set the destination font change.
-    font_change = Font(name='Arial', size=12, color='8B0000',
-                       bold=True, italic=True)
-
-    """
-    Search all of the data to find 'Not available'.
-    This strange error is documented, but all instances of 'Not available'
-    must be a lower case 'a'.
-
-    When 'Not available' is found, set the font change.
-    """
-    for row in ws.iter_rows():
-        for cell in row:
-            if cell.value == 'Not available':
-                # Apply the formatting.
-                cell.font = font_change
-
-def format_data():
-    format_title()
-    center_align_cells(ws)
-    right_align_column_a()
-
-    columns = ['A', 'B', 'C', 'D', 'E']
-    for column in columns:
-        autofit_columns(column)
-
-    check_unavailable_data()
 ##############################################################################
+""" 1. Defining the data class. """
 
 class CapturedData:
-    def __init__(self, task_name, set_part,
-                 parent_build, start_date: None,
-                 end_date: None):
+    def __init__(self, object_name, task_name, set_part,
+                 parent_build, start_date,
+                 end_date):
 
-        # self.object_name = object_name
+        self.object_name = object_name
         self.task_name = task_name
         self.set_part = set_part
         self.parent_build = parent_build
         self.start_date = start_date
         self.end_date = end_date
 
-    def set_start_date(self, date):
-        self.start_date = date
+    def print_status(self):
+        print(F'object_name = {self.object_name}')
+        print(F'task_name = {self.task_name}')
+        print(F'set_part = {self.set_part}')
+        print(F'parent_build = {self.parent_build}')
+        print(F'start_date = {self.start_date}')
+        print(F'end_date = {self.end_date}')
+        
+
     
     def set_start_date(self, date_string):
         date_format = "%Y-%m-%d"
         self.start_date = datetime.strptime(date_string, date_format)
 
-
-
-##############################################################################
-##############################################################################
-""" section of code to delete when ready """
-
-
-column_names = ['Task', 'Set Part', 'Parent Build', 'Start Date', 'End Date']
-# corresponding_keys = ['content', 'entity', 'sg_parent_build',
-#                     'start_date', 'due_date']
-
-# data_entry_1 = CapturedData("task1", "set1", "build1")
-# data_entry_1.set_start_date("2021-01-01")
-# print(data_entry_1.start_date)
-# def add_data_to_excel_file(data, data_file_name):
-#     column_names = []  # Find names for titles that match requirements
-#     get_data_from_lists(sorted_data)
-
-# def check_column_letter_set_data(letter):  # check column and return value
-#     try:
-#         if letter == 'A':
-#             value = 'content'  # Task Name.
-#         elif letter == 'B':
-#             value = 'entity-code'  # Part Name.
-#         elif letter == 'C':
-#             value = 'sg_parent_build-code'  # Parent Build Name.
-#         elif letter == 'D':
-#             value = 'start_date'  # Start Date.
-#         elif letter == 'E':
-#             value = 'due_date'  # End Date.
-#         else:
-#             raise ValueError('There are no columns for this data.')
-#     except ValueError as e:
-#         print('An error occured: ', e)
-#         value = None
-#     # print(value)  # Only for testing purposes---.
-#     return value
-
-
-# def find_specific_key_values(input_dict):  # Find specific key-values.
-#     matching_values = []  # Create a list for values from keys.
-
-#     # Add values from specific keys to list.
-
-#     # 1. Task Name.
-#     task_name = catch_key(input_dict, 'content', None)
-#     matching_values.append(task_name)
-
-#     # 2. Set Part Name.
-#     # Need to remove brackets from  this data point.
-#     set_part = remove_brackets(catch_key(input_dict, 'entity', 'code'))
-#     matching_values.append(set_part)
-
-#     # 3. Parent Build.
-#     p_build = catch_key(input_dict, 'sg_parent_build', 'code')
-#     parent_build = remove_quote(p_build)
-#     matching_values.append(parent_build)
-
-#     # 4. Start Date.
-#     start_date = catch_key(input_dict, 'start_date', None)
-#     matching_values.append(start_date)
-
-#     # 5. End Date.
-#     due_date = catch_key(input_dict, 'due_date', None)
-#     matching_values.append(due_date)
-
-#     # print('values : {}\n'.format(matching_values))  # Testing purposes---.
-#     return matching_values
-
-
-
-
-##############################################################################
-##############################################################################
-##############################################################################
-##############################################################################
-##############################################################################
-
-
-
-
-
-
-
-##############################################################################
-""" 1. Unpickle and sort data by earliest start date, place in list. """
-def remove_brackets(input):
-    return str(input).strip('[]')
-
-
-def remove_quote(input):
-    return str(input).strip(" '' ")
-
-
-def get_value(input_data, key_code):  # Find specific key-values.
-    """
-    This is the main data conversion function.
-
-    The goal is to find the requested data that should be on the
-    spreadsheet.
-
-    When an object is created, it will set object traits to the value it 
-    should be.  This function retreives the value for the associated key.
-
-    Some pieces of data may need to be corrected, this is done by removing
-    brackts "[]" and single quotes around text " '' ".
-    """
-
-    switcher = {
-        'task_name': ('content', None),
-        'set_part': ('entity', 'code'),
-        'parent_build': ('sg_parent_build', 'code'),
-        'start_date': ('start_date', None),
-        'due_date': ('due_date', None)
-    }
-    if key_code not in switcher:
-        return 'Invalid Key (key_code'
-
-    # Find the corresponding key-value pair
-    key_values = switcher.get(key_code) # Get the key-cade value
-
-    # if the pair has a nested list, get the nested value
-    if key_values[1] is not None:
-        returned_key = input_data[key_values[0]][key_values[1]]
-    else:  # get the normal value
-        returned_key = input_data[key_values[0]]
-    # Some data may not exist, 
-    if returned_key is None:
-        returned_key = 'Not available'
-
-    # Fix formatting issues with data, check for [] and '.
-    if key_values[0].startswith('[') and key_values[0].endswith(']'):
-        returned_key = remove_brackets(returned_key)
-
-    if key_values[0].startswith("'") and key_values[0].endswith("'"):
-        returned_key = remove_quote(returned_key)
-
-    return returned_key
-
-
-def set_cell_value(ws, row, col, value):
-    cell = ws.cell(row=row, column=col)
-    cell.value = value
-
+    def set_end_date(self, date_string):
+        date_format = "%Y-%m-%d"
+        self.end_date = datetime.strptime(date_string, date_format)
 
 ##############################################################################
 """ 2. Create an excel file. """
+
+
+def set_cell_value(worksheet, row, col, value):
+    cell = worksheet.cell(row=row, column=col)
+    converted_value = ''.join(value)
+    cell.value = converted_value
 
 
 def create_excel_file(data_file_name):
@@ -353,114 +116,33 @@ def create_excel_file(data_file_name):
     ws = wb.active  # Add worksheet.
     ws.title = 'Formatted'  # Change title.
 
+    # Pass titles from column_names list to excel.
+    for i in range(1,(len(column_names)+1)):
+        title = str(column_names[i-1])
 
-##############################################################################
-""" 3. Add data to excel file after fixing some data. """
+        set_cell_value(ws, 1, i, title)
 
-"""
-This section of code is all about taking the input data from the .pkl
-file and converting it into simple lists.  The lists will then be added to
-excel.  Some issues will arise as some of the input data may return None.
-"""
-
-def set_title_names_in_excel(input_names):  # Pass titles from list to excel.
-    column_index = 0  # Setting counter iter for stringed loop.
-
-    for name in input_names:
-        # print(name)  # Only for testing purposes---.
-        column_index += 1
-        # column_letter = get_column_letter(column_index)
-        # column_index = column_index_from_string(name)
-        ws.cell(row=1, column=column_index, value=name)
-
-
-
-
-
-def create_new_data_objects(data_in):
-    print('####################------##############')
-    # Convert pickled data into a list.
-    unpickled_data = open(data_in, 'rb')
-    data = pickle.load(unpickled_data)
-
-    # Sort data by start date now
-    sorted_data = sorted(data, key=lambda x: x['start_date'])
-
-    # Find size - how many lists are inside this list.
-    list_size = len(sorted_data)
-    print('Size of list = {}\n'.format(list_size))
-
-    # List of objects created from data
-    captured_data_list = []
-    for i in range(0, list_size):
-        # object_name = ('object_' + str(i))
-        captured_data_list.append(CapturedData('','','','',''))
-
-    for index, objects in enumerate(captured_data_list):
-        """
-        For each object, set specific_data to list index
-        The List index is which data entry point in the larger list.  Then set
-        the object attribute to the key:value pair
-        """
-        specific_data = sorted_data[index]
-        objects.task_name = get_value(specific_data, 'task_name')
-        objects.set_part = get_value(specific_data, 'set_part')
-        objects.parent_build = get_value(specific_data, 'parent_build')
-        objects.start_date = get_value(specific_data, 'start_date')
-        objects.end_date = get_value(specific_data, 'due_date')
-        
-        set_cell_value(ws, objects, 'A', objects.task_name)
-        set_cell_value(ws, objects, 'B', objects.set_part)
-        set_cell_value(ws, objects, 'C', objects.parent_build)
-        set_cell_value(ws, objects, 'D', objects.start_date)
-        set_cell_value(ws, objects, 'E', objects.end_date)
-
-
-
-##############################################################################
-"""
-Main function to process data with script. 
-
-An input file needs to be processed and automated so it creates and fills a 
-worksheet in excel.
-"""
-
-def process_data(file_to_process):
-    # Create an excel file using the file name of the data to be processed.
-    create_excel_file(file_to_process)
+    # Create file name and save file.
+    output_file = (data_file_name + '_converted_data.xlsx')
+    wb.save(output_file)
     
-    # Create objects for each row of data.  Place data in excel.
-    create_new_data_objects(file_to_process)
-    
-    set_title_names_in_excel(column_names)
 
 ##############################################################################
 ##############################################################################
 
 if __name__ == '__main__':
     print('\nStarting Script\n')  # Only for testing purposes--------------.
-
+    a = CapturedData('a1', 'build', 'roof', 'horace', '2016-12-05', '2017-1-23')
+    a.print_status()
+    
     # Input file when script is run through command line
     input_file = sys.argv[1]  # 'test_data.pkl'
-    output_file = (input_file + '_converted_data.xlsx')
+    create_excel_file(input_file)
 
     # Unpickle, sort, and set up excel file.
-    process_data(input_file)
+    # process_data(input_file)
 
     # Select data in excel and format it.
-    format_data()
-
-    wb.save(output_file)
+    # format_data()
+    
     print('\nFinished Processing')  # Only for testing purposes----------.
-
-"""
-Basic command line template:
-
-python3 this_script.py data_to_process.pkl
-
-Note: 
-Include full path
-
-python3 /convert_to_excel.py  /test_data.pkl 
-
-"""
