e41bc8eb58372ae0ed17bc32f7056e327122edc8
drewgillievfx <drewgillievfx@gmail.com>
2023-01-31 00:36:37 -0500

    Creating Files and adding comments

---
+++ BONUS_CHALLENGE/ocio_challenge_2.py
@@ -0,0 +1,11 @@
+"""
+ocio_challenge_2.py
+Start Date: 20230131
+Version: 1.0
+Written by: Drew Gillie
+
+2. Write a script that uses dcraw or other command line raw developing software to process the provided raw file.
+    - To the best of my knowledge there isn't a python solution here - you will need to use subprocess to execute a command.
+    - Convert from .cr2 to ACEScg .exr file using dcraw - commit a sample file for verification
+    - Convert that ACEScg exr file to an sRGB .jpeg - commit a sample file for verification
+"""
---
+++ BONUS_CHALLENGE/oiio_challenge_1.py
@@ -0,0 +1,18 @@
+"""
+oiio_challenge_1.py
+Start Date: 20230131
+Version: 1.0
+Written by: Drew Gillie
+
+1. Write a script that uses OIIO/OCIO to do the following operations on laika_framing_chart.exr
+    - Crop from 2348x1566 to 2048x858
+    - Matte the 2048x858 image into a 2048x1080 frame with black bars top and bottom
+    - Output this resulting 2048x1080 image as an sRGB jpeg - commit the output jpeg for verification
+"""
+
+
+import OpenImageIO as oiio
+
+
+print(F'\nSTARTING SCRIPT------------\n')
+print(F'\nFINISHED SCRIPT------------\n')
